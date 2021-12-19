cf27415899379fdae4dcf2c741a83e34a95e9b79
Max Marchuk <themaxmarchuk@gmail.com>
2021-12-19 14:36:21 -0600

    Use lua init file instead of vimscript

---
+++ plugin/tailwindcss-colors.lua
@@ -0,0 +1,10 @@
+if vim.g.loaded_tailwindcss_colors == 1 then
+   return
+end
+
+vim.g.loaded_tailwindcss_colors = 1
+
+vim.cmd "command! TailwindColorsAttach lua require('tailwindcss-colors').buf_attach()"
+vim.cmd "command! TailwindColorsDetach lua require('tailwindcss-colors').buf_detach()"
+vim.cmd "command! TailwindColorsToggle lua require('tailwindcss-colors').buf_toggle()"
+vim.cmd "command! TailwindColorsRefresh lua require('tailwindcss-colors').update_highlight(vim.api.nvim_get_current_buf())"
